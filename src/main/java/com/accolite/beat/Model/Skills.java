f589243ff3ba81f14f9bcbfe999a32248354d205
niharikasargam30 <niharika.sargam@accolitedigital.com>
2022-12-22 17:02:31 +0530

    Updated SkillsAndExperience Entities issue id #5

--- src/main/java/com/accolite/beat/Model/Skills.java
+++ src/main/java/com/accolite/beat/Model/Skills.java
@@ -3,14 +3,15 @@ package com.accolite.beat.Model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NonNull;
+
 
 @Data
 @Entity
+@Table(name = "SKILLS")
 public class Skills {
    @Id
    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private int Id;
-   @NonNull
-    private String Skill;
+    private int id;
+
+    private String skill;
 }
