a14a13f9b9a77ea90b3b637229d21772e969f4b3
niharikasargam30 <niharika.sargam@accolitedigital.com>
2022-12-23 12:57:03 +0530

    created repos for client, employee, history, expecience slab, login and organization #24

--- src/main/java/org/accolite/db/repo/ClientCounterpartRepository.java
+++ src/main/java/org/accolite/db/repo/ClientCounterpartRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientCounterpartRepository extends JpaRepository<ClientCounterpart,Long> {
+public interface ClientCounterpartRepository extends JpaRepository<ClientCounterpart,Long>{
 }
--- src/main/java/org/accolite/db/repo/EmployeeHistoryRepository.java
+++ src/main/java/org/accolite/db/repo/EmployeeHistoryRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeHistoryRepository extends JpaRepository<EmployeeHistory,Long>{
-
 }
