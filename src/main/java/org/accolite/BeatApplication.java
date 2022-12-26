c297587ec8c21f8990ccce69e13b906519044e38
Acco-ManishKumar <manish.kumar@accolitedigital.com>
2022-12-26 14:19:39 +0530

    Implement profile based Logback file for dev and prod  #26

--- src/main/java/org/accolite/BeatApplication.java
+++ src/main/java/org/accolite/BeatApplication.java
@@ -1,15 +1,17 @@
 package org.accolite;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BeatApplication {
-
 	public static void main(String[] args) {
+		Logger log = LoggerFactory.getLogger(BeatApplication.class);
 		SpringApplication.run(BeatApplication.class, args);
-		System.out.print("hello world");
+		log.debug("hello printed");
+		System.out.println("hello world");
 	}
-
 }
