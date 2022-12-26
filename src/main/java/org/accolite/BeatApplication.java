10c37e754b0ab45f018acc2b5200a406189666e3
Acco-ManishKumar <manish.kumar@accolitedigital.com>
2022-12-26 15:07:48 +0530

    updated logback with lombok #26

--- src/main/java/org/accolite/BeatApplication.java
+++ src/main/java/org/accolite/BeatApplication.java
@@ -1,17 +1,16 @@
 package org.accolite;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BeatApplication {
 	public static void main(String[] args) {
-		Logger log = LoggerFactory.getLogger(BeatApplication.class);
 		SpringApplication.run(BeatApplication.class, args);
-		log.debug("hello printed");
+		log.debug("hello world printed");
 		System.out.println("hello world");
 	}
 }
