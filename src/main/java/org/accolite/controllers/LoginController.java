9cc2febbcc8d7cb97fd2fdb05256c68838bb7ace
Acco-ManishKumar <manish.kumar@accolitedigital.com>
2023-01-01 15:55:00 +0530

    Implemented logs in login controller #44

--- src/main/java/org/accolite/controllers/LoginController.java
+++ src/main/java/org/accolite/controllers/LoginController.java
@@ -9,6 +9,8 @@ import org.accolite.pojo.LoginDetails;
 import org.accolite.pojo.SessionDetails;
 import org.accolite.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.ObjectFactory;
@@ -27,24 +29,27 @@ public class LoginController {
     @Autowired
     ObjectFactory<HttpSession> httpSessionFactory;
 
-    @PostMapping(value = PathConstants.loginPath, consumes = "application/json", produces = "plain/text")
+    @PostMapping(value = PathConstants.loginPath, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
     private ResponseEntity<String> login(@RequestBody LoginDetails loginDetailsFromClient) {
         HttpSession session = httpSessionFactory.getObject();
         boolean isValidOrInvalidUser = loginComponent.checkLoginDetailsFromClient(loginDetailsFromClient, session);
         if(isValidOrInvalidUser){
             if(rolesGroupComponent.setAccess(session)){
+                log.debug("User logged in = "+((SessionDetails) session.getAttribute("sessionDetailsInSession")));
                 return ResponseEntity.ok().body("User logged in = "+((SessionDetails) session.getAttribute("sessionDetailsInSession")).toString());
             }
             else {
-                return ResponseEntity.internalServerError().body("Access is unable to set for this session");
+                log.debug("Access is unable to set for this session");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Access is unable to set for this session");
             }
         }
         else {
-            return ResponseEntity.notFound().build();
+            log.debug("Invalid user details");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Invalid user details");
         }
     }
 
-    @GetMapping(value = PathConstants.accessPath,produces = "plain/text")
+    @GetMapping(value = PathConstants.accessPath,produces = MediaType.TEXT_PLAIN_VALUE)
     private  ResponseEntity<String> access(@RequestHeader("jwt") String token){
         HttpSession session = httpSessionFactory.getObject();
         SessionDetails sessionDetails = (SessionDetails) session.getAttribute("sessionDetailsInSession");
@@ -52,17 +57,21 @@ public class LoginController {
             if(jwtUtil.validateToken(token,String.valueOf(sessionDetails.getEmpId()))){
                 Access access = Access.getInstance();
                 if(access.access == null){
-                    return ResponseEntity.notFound().build();
+                    log.debug("Access is not defined in current session");
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Access is not defined in current session");
                 }
                 else {
+                    log.debug("Access is retrieved successfully");
                     return ResponseEntity.ok().body(access.access);
                 }
             }
             else {
-                return ResponseEntity.badRequest().build();
+                log.debug("invalid jwt token");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid jwt token");
             }
         }catch (Exception ex){
-            return ResponseEntity.badRequest().build();
+            log.debug("jjwt throw exception"+ex);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("jjwt throw exception"+ex);
         }
     }
 }
